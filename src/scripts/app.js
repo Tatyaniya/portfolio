aee7c9a9577e2d0251f2a8447332c95f68f153f0
Tatyaniya <tatyaniya@bigmir.net>
2018-01-01 22:56:42 +0200

    parallax

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,24 @@
-const slider = require('./common/slider');
-const $ = require('jquery'); // если будет нужен
+// parallax
 
-slider(); // инициализируем слайдер
+const parallaxContainer = document.getElementById('parallax');
+const layers = parallaxContainer.children;
+
+const moveLayers = function (e) {
+    const initialX = (window.innerWidth / 2) - e.pageX;
+    const initialY = (window.innerHeight / 2) - e.pageY;
+
+    let i = 0;
+    for (let layer of layers) {
+        const divider = 1/40;
+        const positionX = initialX * divider;
+        const positionY = initialY * divider;
+        const bottomPosition = (window.innerHeight / 2) * divider;
+        const image = layer.firstElementChild;
+
+        layer.style.transform = `translate(${positionX}px, ${positionX}px)`;
+        image.style.bottom = `-${bottomPosition}px`;
+        i++;
+    }
+};
+
+window.addEventListener('mousemove', moveLayers);
