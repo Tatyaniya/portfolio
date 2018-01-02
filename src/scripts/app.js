0b8d78b535572d6fe208825c4180c431763e4b3b
Tatyaniya <tatyaniya@bigmir.net>
2018-01-02 18:50:47 +0200

    map

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -22,3 +22,17 @@ const moveLayers = function (e) {
 };
 
 window.addEventListener('mousemove', moveLayers); 
+
+
+// map 
+
+function initMap()
+{
+    var element = document.getElementById('map');
+    var options = {
+        zoom: 5,
+        center: {lat: 50.4501, lng: 30.5234}
+    };
+
+    var myMap = new google.maps.Map(element, options);
+}
